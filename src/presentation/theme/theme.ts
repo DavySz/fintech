cabc1c30ec3bf08ce675e8776ca6f9db781fbb32
Davy Assunção <davydesouzabar@gmail.com>
2023-07-02 17:02:48 -0400

    feat: set default themes

---
+++ src/presentation/theme/theme.ts
@@ -0,0 +1,31 @@
+export const theme = {
+    colors: {
+        neutrals: {
+            inverted: "#F5F5F5",
+            weakness: "#BBAABB",
+            default: "#111111",
+            weak: "#BABEC9",
+        },
+        orange: {
+            default: "#FF872C",
+        },
+        purple: {
+            default: "#5525BB",
+        },
+        green: {
+            default: "#12A454",
+        },
+        red: {
+            default: "#E83F5B",
+        },
+    },
+    fonts: {
+        poppins: "'Poppins', sans-serif",
+    },
+    weight: {
+        regular: 400,
+        medium: 500,
+        semiBold: 600,
+        bold: 700,
+    },
+};
