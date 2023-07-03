45c7bae4cbb23cc232e4dc22290364ce16e37518
Davy Assunção <davydesouzabar@gmail.com>
2023-07-03 16:49:08 -0400

    fix: eslint format rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
-  );
+import { type ReactNode } from "react";
+import { ThemeProvider } from "styled-components";
+
+import { SignIn } from "./presentation/pages/sign-in/sign-in";
+import { defaultTheme } from "./presentation/themes/themes";
+
+function App(): ReactNode {
+    return (
+        <ThemeProvider theme={defaultTheme}>
+            <SignIn />
+        </ThemeProvider>
+    );
 }
 
 export default App;
