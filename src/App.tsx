60acac974bf2e53247c92140e540860b15175190
Davy Assunção <davydesouzabar@gmail.com>
2023-07-04 01:28:25 -0400

    feat: use application routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import { type ReactNode } from "react";
 import { ThemeProvider } from "styled-components";
 
 import { defaultTheme } from "./presentation/themes/themes";
-import { SignUp } from "./presentation/pages/sign-up/sign-up";
+import { ApplicationRoutes } from "./presentation/routes/routes";
 
 function App(): ReactNode {
     return (
         <ThemeProvider theme={defaultTheme}>
-            <SignUp />
+            <ApplicationRoutes />
         </ThemeProvider>
     );
 }
