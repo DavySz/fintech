c154c74319dd4f5d55144a689d035f44de84d670
davy <davydesouzabar@gmail.com>
2023-11-17 02:07:33 -0400

    chore: config recoil state management

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@ import { ThemeProvider } from "styled-components";
 
 import { defaultTheme } from "./presentation/themes/themes";
 import { ApplicationRoutes } from "./main/routes/routes";
+import { RecoilRoot } from "recoil";
 
 function App(): ReactNode {
     return (
         <ThemeProvider theme={defaultTheme}>
-            <ApplicationRoutes />
+            <RecoilRoot>
+                <ApplicationRoutes />
+            </RecoilRoot>
         </ThemeProvider>
     );
 }
