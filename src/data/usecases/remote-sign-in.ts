7b9a7ef692b49406804695425908fe5b05166f47
davy <davydesouzabar@gmail.com>
2023-11-17 02:11:19 -0400

    feat: implement sign in case

--- src/data/usecases/remote-sign-in.ts
+++ src/data/usecases/remote-sign-in.ts
@@ -1,1 +1,30 @@
-export class RemoteSign {}
+import { UserModel } from "../../domain/models";
+import { SignInCase, SignInSpace } from "../../domain/usecases/remote-sign-in";
+import { Encrypt } from "../crypto";
+import { IHttpClient } from "../protocols/http";
+
+export class RemoteSign implements SignInCase {
+    constructor(
+        private readonly httpClient: IHttpClient<UserModel>,
+        private readonly encrypt: Encrypt,
+        private readonly url: string
+    ) {}
+
+    async signIn(params: SignInSpace.Params): Promise<SignInSpace.Model> {
+        const key = String(process.env.ENCRYPT_SECRET);
+        const passwordEncrypted = this.encrypt.encrypt(params.password, key);
+
+        const body: SignInSpace.Params = {
+            password: passwordEncrypted,
+            email: params.email,
+        };
+
+        const response = await this.httpClient.request({
+            url: this.url,
+            method: "get",
+            body,
+        });
+
+        return response.body;
+    }
+}
---
+++ src/main/factories/pages/index.ts
@@ -0,0 +1,2 @@
+export * from "./sign-up.factory";
+export * from "./sign-in.factory";
