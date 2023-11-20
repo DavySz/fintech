69a54851bb92b83ce0f928230fb4dad7c70aa68e
davy <davydesouzabar@gmail.com>
2023-11-20 02:11:26 -0400

    feat: add correct backend urls

--- src/main/factories/usecases/remote-get-conciliation.factory.ts
+++ src/main/factories/usecases/remote-get-conciliation.factory.ts
@@ -1,12 +1,16 @@
+import { useRecoilState } from "recoil";
 import { RemoteGetConciliation } from "../../../data/usecases";
 import {
     GetConciliationCase,
     GetConciliationSpace,
 } from "../../../domain/usecases";
 import { makeAxiosHttpClientAdapter } from "../http";
+import { userState } from "../../../presentation/recoil/atoms/user.atom";
 
 export const makeRemoteGetConciliation = (): GetConciliationCase => {
-    const url = String(process.env.BFF_URL);
+    const [user, _] = useRecoilState(userState);
+
+    const url = `${process.env.BFF_URL}/conciliation/${user.user_id}`;
 
     const axiosHttpClientAdapter =
         makeAxiosHttpClientAdapter<GetConciliationSpace.Model[]>();
